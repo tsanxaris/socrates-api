5bb91d52e6b68ea708f77b773465dd3ecbfbedc3
tsanxaris <61061727+tsanxaris@users.noreply.github.com>
2026-04-25 18:44:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,7 @@ app.post("/ai", async (req, res) => {
     const data = await response.json();
 
     // ΣΤΕΛΝΟΥΜΕ ΜΟΝΟ ΤΟ ΚΕΙΜΕΝΟ
-res.json({
-  text: data.choices[0].message.content
-});
+res.send(data.choices[0].message.content);
 
   } catch (error) {
     res.send("Error generating response");
